152ee4b04489ed47d4d2360fa5f17ba224d5fd4c
shrafibd@gmail.com <shrafibd@gmail.com>
2024-10-30 16:02:10 +0600

    Write login script

--- AutoTube.py
+++ AutoTube.py
@@ -6,7 +6,27 @@ import time
 
 driver = webdriver.Firefox()
 
+login_upload_url = "https://rumble.com/login.php?next=%2Fupload.php"
+username = "un"
+password = "pw"
+
+
 try:
-    driver.get("https://www.google.com")
+    #Browse the upload page. We have to login first. Afterwords the link will take us to the upload page
+    driver.get(login_upload_url)
+    time.sleep(10)
+    
+    #Input username
+    username_field = driver.find_element(By.ID, "login-username")
+    username_field.send_keys(username)
+    
+    #Input password
+    password_field = driver.find_element(By.ID, "login-password")
+    password_field.send_keys(password)
+    time.sleep(10)
+    
+    #lets login
+    login_button = driver.find_element(By.XPATH, '//button[text()="Sign in"]')
+    login_button.click()
 finally:
     print("done")
